07739d70f3c0e0b5a25d9086e1f77f4409247a9f
bug-yu <69247804+bug-yu@users.noreply.github.com>
2024-07-12 18:25:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.bigyu.eu.org": dockerHub,
+  "quay.bigyu.eu.org": "https://quay.io",
+  "gcr.bigyu.eu.org": "https://gcr.io",
+  "k8s-gcr.bigyu.eu.org": "https://k8s.gcr.io",
+  "k8s.bigyu.eu.org": "https://registry.k8s.io",
+  "ghcr.bigyu.eu.org": "https://ghcr.io",
+  "cloudsmith.bigyu.eu.org": "https://docker.cloudsmith.io",
+  "ecr.bigyu.eu.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.bigyu.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
